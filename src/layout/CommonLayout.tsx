173985909644b503079e271f880300cf807ee2ad
Rdm-jony <jonydascse@gmail.com>
2025-08-28 14:16:43 +0600

    basic project setup

---
+++ src/layout/CommonLayout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const CommonLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="min-h-svh flex flex-col">
+            <Navbar />
+            <div className="grow">
+                {children}
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default CommonLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './routes/index.tsx'
+import { store } from './redux/store.ts'
+import { Provider as ReduxProvider } from 'react-redux'
+import { ThemeProvider } from './provider/theme.provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
